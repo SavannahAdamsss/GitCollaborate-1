b938fe17acd732ec92914701254b07c35f1a5a40
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-05 14:05:28 -0600

    numbers

--- app/app.js
+++ app/app.js
@@ -1,4 +1,14 @@
-let students = [
-  {'Will'},
-  {27}
-]
+var students = [
+  {name :'Will', 
+  age: 27},
+  {name :'Savannah', 
+  age :31}
+];
+
+var multiply = function () {
+  var numbers = [2, 4, 7, 9];
+};
+
+
+
+
